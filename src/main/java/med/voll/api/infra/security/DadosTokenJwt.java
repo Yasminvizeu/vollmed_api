bbb78637174ad25db253ae1239dcf29149dabb0e
Yasmin Vizeu Gil <yasmin.gil@meta.com.br>
2023-06-06 14:00:52 -0300

    criando filtro

---
+++ src/main/java/med/voll/api/infra/security/DadosTokenJwt.java
@@ -0,0 +1,6 @@
+package med.voll.api.infra.security;
+
+import org.antlr.v4.runtime.Token;
+
+public record DadosTokenJwt(String tokent) {
+}
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,8 +4,10 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -13,9 +15,12 @@ import java.util.Date;
 
 @Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret; //criado no porperties api.security.token.secret={JWT_SECRET} por estar entre chaves o spring sabe que deve procurar uma variavel de ambiente no computador
     public String gerarToken(Usuario usuario){
         try {
-            var algoritimo = Algorithm.HMAC256("12345678"); //PRECISA DE SENHA PRA USAR O algoritimo
+            var algoritimo = Algorithm.HMAC256(secret); //PRECISA DE SENHA PRA USAR O algoritimo
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())// forma de identificar os usuarios
