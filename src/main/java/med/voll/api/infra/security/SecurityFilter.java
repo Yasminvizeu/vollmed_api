380fda7fdfe344a96c1c31b2b35022f7ad8cb0f2
Yasmin Vizeu Gil <yasmin.gil@meta.com.br>
2023-06-07 11:16:30 -0300

    autenticando o usuario

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,21 +17,37 @@ import java.io.IOException;
 @Component //classes genericas
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    var tokenJWT = recuperarToken(request);
+        var tokenJWT = recuperarToken(request);
 
-        System.out.println(tokenJWT); //teste
-    //SEGUE O FLUXO DA REQUISIÇÃO
+        if(tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByLogin(subject); // pra acessar o banco de dados precisa do repository
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+
+
+        //SEGUE O FLUXO DA REQUISIÇÃO
         filterChain.doFilter(request,response);
     }//dar um system.out pra garantir que filro é executado apenas uma unica vez por request
 
     private String recuperarToken(HttpServletRequest request) {
 
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("Token JWT nao enviado no cabeçalho authorization ");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer", "");
         }
-        return authorizationHeader.replace("Bearer", "");
+        return null;
+
     }
 }
